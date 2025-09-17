ec20ce311663058bfabdf7b109c45108250b4f7e
vishnudivya2020 <vishnudivyabharathi@gmail.com>
2025-09-17 21:33:30 +0530

    Add a chenges in InvitePage

--- app/accept-invite/page.jsx
+++ app/accept-invite/page.jsx
@@ -2,7 +2,7 @@
 import { Suspense } from "react";
 import AcceptInviteComponent from "./AcceptInviteComponent";
 
-export default function AcceptInvitePageWrapper() {
+export default function AcceptInvitePage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <AcceptInviteComponent />
